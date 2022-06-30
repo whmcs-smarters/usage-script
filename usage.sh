c6c58f5fe2d59d0559413816fecddf1e1f612ee7
Amanpreet Singh <support@whmcssmarters.com>
2022-06-30 15:16:19 +0530

    /tmp/speed issue fixed

--- usage.sh
+++ usage.sh
@@ -1,35 +1,31 @@
-#! /bin/bash
-CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%", $(NF-2)}')
+#!/bin/bash
+CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
 echo -e '\E[32m'"CPU Load Average :" $CPU
-MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%", $3*100/$2 }')
+MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
 echo -e '\E[32m'"Memory usage :" $MEMORY
-DISK=$(df -h | awk '$NF=="/"{printf "%s", $5}')
+DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
 echo -e '\E[32m'"Disk Usage :" $DISK
 tecuptime=$(uptime | awk '{print $3,$4}' | cut -f1 -d,)
 echo -e '\E[32m'"System Uptime Days/(HH:MM) :" $tecuptime
 echo "Calculating Bandwidth Usage"
-vnstat -tr -i $(ip route get 1.1.1.1 | awk -- '{printf $5}') > /tmp/usage
-upload=$(cat /tmp/usage | grep -i "tx")
-download=$(cat /tmp/usage | grep -i "rx")
+eth0=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
+vnstat -tr -i $eth0 >> /tmp/usage.txt
+upload=$(cat /tmp/usage.txt | grep -i "tx")
+download=$(cat /tmp/usage.txt | grep -i "rx")
 upload2=$(echo $upload | cut -d" " -f2,3)
 download2=$(echo $download | cut -d" " -f2,3)
 echo "Current update data trasfer  speed :" $upload2
 echo "Current downlod  data trasfer Speed :" $download2
-vnstat -t -s -i $(ip route get 1.1.1.1 | awk -- '{printf $5}') > /tmp/usage2
-totald=$(cat /tmp/usage2 | grep -i "today" | cut -f3 -d"/")
-#echo  -e '\E[32m' "Total Data Usage : " $totald
+vnstat -t -s -i $eth0 >> /tmp/usage2.txt
+totald=$(cat /tmp/usage2.txt | grep -i "today" | cut -f3 -d"/")
+#echo  -e '\E[32m' "Total Data Usage:" $totald
 totald2=$(echo $totald | tr -d ' ')
 echo  -e '\E[32m' "Total Data Usage:" $totald2
 rm -Rf /tmp/usage*
 echo "Calculating Download and Upload Speed on Server"
-curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -  > /tmp/speed
-speed1=$(cat /tmp/speed | grep -i "Download:")
-speed2=$(cat /tmp/speed |grep -i "Upload:")
+curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -  >> /tmp/speed.txt
+speed1=$(cat /tmp/speed.txt | grep -i "Download:")
+speed2=$(cat /tmp/speed.txt |grep -i "Upload:")
 echo $speed1
 echo $speed2
-rm -Rf /tmp/speed
-
-cat /dev/null > /var/www/usage/index.html  # clear first
-cat >> /var/www/usage/index.html <<EOF
-$CPU,$MEMORY,$DISK,$tecuptime,$upload2,$download2,$totald2,$speed1,$speed2
-EOF
+rm -Rf /tmp/speed*
