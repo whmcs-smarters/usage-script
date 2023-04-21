93c65995c3444f8e54d9ca0146f86f74dbdd819e
Amanpreet Singh <support@whmcssmarters.com>
2023-04-21 17:36:43 +0530

    Update check_services.sh

--- check_services.sh
+++ check_services.sh
@@ -1,34 +1,24 @@
 #!/bin/bash
 
 # Define file paths for OpenVPN and IKEv2 status
-if [ -e /var/www/status/openvpn.txt ]
-then
-   OPENVPN_STATUS_FILE="/var/www/status/openvpn.txt"
-else
-    mkdir -p /var/www/status/
-    touch /var/www/status/openvpn.txt
-    OPENVPN_STATUS_FILE="/var/www/status/openvpn.txt"
-fi
 
-if [ -e /var/www/status/ikev2.txt ]
-then
-  IKEV2_STATUS_FILE="/var/www/status/ikev2.txt"
-else
-    mkdir -p /var/www/status/
-    touch /var/www/status/ikev2.txt
-    IKEV2_STATUS_FILE="/var/www/status/ikev2.txt"
-fi
+OPENVPN_STATUS_FILE="/var/www/usage/openvpn.txt"
+IKEV2_STATUS_FILE="/var/www/usage/ikev2.txt"
 
 # Check if the OpenVPN service is running
-if systemctl is-active openvpn.service > /dev/null 2>&1; then
+if systemctl is-active openvpn@server > /dev/null 2>&1; then
   echo "online" > $OPENVPN_STATUS_FILE
+  OPENVPN_STATUS="online"
 else
   echo "offline" > $OPENVPN_STATUS_FILE
+  OPENVPN_STATUS="offline"
 fi
 
 # Check if the IKEv2 service is running
 if systemctl is-active strongswan.service > /dev/null 2>&1; then
   echo "online" > $IKEV2_STATUS_FILE
+  IKEV2_STATUS = "online"
 else
   echo "offline" > $IKEV2_STATUS_FILE
+  IKEV2_STATUS = "offline"
 fi
