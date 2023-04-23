1d716c3553ce7cb5530040c80ae49e4d9f2ec7d1
Amanpreet Singh <support@whmcssmarters.com>
2023-04-23 13:29:24 +0530

    Update restart_services.sh

--- restart_services.sh
+++ restart_services.sh
@@ -4,11 +4,9 @@ if systemctl restart openvpn@server > /dev/null 2>&1; then
 echo "Success: OpenVPN Service Restarted Successfully"
 else
   echo "Failed: Failed to Restart OpenVPN Service"  
-  echo "Error Message: $openvpn"
 fi
 if service ipsec restart > /dev/null 2>&1; then
   echo "Success: iPsec Service Restarted Successfully"
 else
    echo "Failed: Failed to Restart iPsec Service" 
-   echo "Error Message: $ipsec"
 fi
